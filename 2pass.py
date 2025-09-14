865878292f57b131762a8269e9481319d168254d
Ultramicroscope <76135269+Ultramicroscope@users.noreply.github.com>
2025-09-14 00:35:15 -0400

    fix: magic\cdot magic=banality

--- 2pass.py
+++ 2pass.py
@@ -19,7 +19,7 @@ temp = f'{f_in}_video_stream.h264.temp'
 
 try:
     subprocess.run(['ffmpeg', '-i', f_in, '-map', '0:v:0', '-c', 'copy', '-f', 'data', temp], check=True, capture_output=True)
-    subprocess.run(['ffmpeg', *(c := ['-y', '-i', f_in, '-c:v', 'libx264', '-b:v', str(8 * (2**20 * parser.parse_args().size - f_in.stat().st_size + Path(temp).stat().st_size - 100_000) / float(subprocess.check_output(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', f_in], text=True))), '-passlogfile', f'{f_in}_ffmpeg2pass']), '-pass', '1', '-an', '-f', 'mp4', os.devnull], check=True)
+    subprocess.run(['ffmpeg', *(c := ['-y', '-i', f_in, '-c:v', 'libx264', '-b:v', str(8 * (1006632 * parser.parse_args().size - f_in.stat().st_size + Path(temp).stat().st_size) / float(subprocess.check_output(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', f_in], text=True))), '-passlogfile', f'{f_in}_ffmpeg2pass']), '-pass', '1', '-an', '-f', 'mp4', os.devnull], check=True)
     subprocess.run(['ffmpeg', *c, '-pass', '2', '-c:a', 'aac', f_in.with_name(f"{f_in.stem}_4pass.mp4")], check=True)
 except Exception as e:
     sys.exit(f'error: {e}')
@@ -27,3 +27,4 @@ finally:
     os.remove(temp)
     for log in Path.cwd().glob(f'{f_in}_ffmpeg2pass*'):
         log.unlink(missing_ok=True)
+
